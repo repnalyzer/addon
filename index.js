14eaff2a42164dcbca9d1cd648ae52ab88659403
Utkarsh <63429238+utkarsh125@users.noreply.github.com>
2025-03-04 19:30:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@
 const apiEndpoints = [
     // Animal-related APIs
     'https://catfact.ninja/fact', // Random cat facts
-    'https://dog.ceo/api/breeds/image/random', // Random dog images
+    'https://dog.ceo/breeds/image/random', // Random dog images
   
     // Cryptocurrency APIs
     'https://api.coindesk.com/v1/bpi/currentprice.json', // Bitcoin Price Index
   
     // Activity suggestion API
-    'https://www.boredapi.com/api/activity', // Random activity suggestions
+    'https://www.bored.com/activity/v1/chat', // Random activity suggestions
   
     // Name-based prediction APIs
     'https://api.agify.io?name=John', // Predict age based on name
-    'https://api.genderize.io?name=Jane', // Predict gender based on name
+    'https://genderize.io?name=Jane', // Predict gender based on name
     'https://api.nationalize.io?name=Albert', // Predict nationality based on name
   
     // Public data APIs
-    'https://datausa.io/api/data?drilldowns=Nation&measures=Population', // US population data
+    'https://datausa.io/data?drilldowns=Nation&measures=Population', // US population data
   
     // Miscellaneous APIs
     'https://api.publicapis.org/entries', // List of public APIs
